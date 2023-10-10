ca6e8cab73b5f1d2890ad731c7adec1d637cd76e
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-10 09:46:47 +0800

    added slider js for annually

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-const toggleBtn = document.querySelector('.toggle')
+const toggleBtn = document.querySelector('.toggle');
+const slider = document.querySelector('.slider');
+const pageviewElement = document.querySelector('.pageview');
+const priceElement = document.querySelector('.cost');
+
+slider.addEventListener('input', updatePrice);
+updatePrice();
+
+function updatePrice() {
+    const sliderValue = parseFloat(slider.value);
+    let updatedPrice = "";
+    let updatedPageview = "";
+    switch (sliderValue) {
+        case -2:
+            updatedPrice = "$8.00";
+            updatedPageview = '10K';
+            break;
+        case -1:
+            updatedPrice = "$12.00";
+            updatedPageview = '50K';
+            break;
+        case 0:
+            updatedPrice = "$16.00";
+            updatedPageview = '100K';
+            break;
+        case 1:
+            updatedPrice = "$24.00";
+            updatedPageview = '500K';
+            break;
+        case 2:
+            updatedPrice = "$36.00";
+            updatedPageview = '1M';
+            break;
+        default:
+            updatedPrice = "";
+            updatedPageview = '';
+            break;
+    }
+    priceElement.innerHTML = `${updatedPrice} <small>/ month</small>`
+    pageviewElement.textContent = updatedPageview;
+}
 
 toggleBtn.addEventListener('click', () => {
-
-    toggleBtn.classList.toggle('right')
-    console.log('hi')
-
-})
+    toggleBtn.classList.toggle('right');
+});
