18ae40b1825f953a9de8ff734b6362d236fdb77a
Asliddin Amirov <asliddinamir2@gmail.com>
2023-10-10 09:56:30 +0800

    last commit

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const pageviewElement = document.querySelector('.pageview');
 const priceElement = document.querySelector('.cost');
 
 slider.addEventListener('input', updatePrice);
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('right');
+    updatePrice();
+});
 updatePrice();
 
 function updatePrice() {
@@ -11,32 +15,38 @@ function updatePrice() {
     let updatedPrice = "";
     let updatedPageview = "";
     let background = "";
+    const isRightToggle = toggleBtn.classList.contains('right');
 
     switch (sliderValue) {
         case -2:
-            updatedPrice = "$8.00";
+            updatedPrice = isRightToggle ? "$72.00" : "$8.00";
             updatedPageview = '10K';
-            background = 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 0%, rgb(234, 238, 251) 0%, rgb(234, 238, 251) 100%)';
+            background = isRightToggle ? 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 0%, rgb(234, 238, 251) 0%, rgb(234, 238, 251) 100%)' :
+                'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 0%, rgb(234, 238, 251) 0%, rgb(234, 238, 251) 100%)';
             break;
         case -1:
-            updatedPrice = "$12.00";
+            updatedPrice = isRightToggle ? "$108.00" : "$12.00";
             updatedPageview = '50K';
-            background = 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 25%, rgb(234, 238, 251) 25%, rgb(234, 238, 251) 100%)';
+            background = isRightToggle ? 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 25%, rgb(234, 238, 251) 25%, rgb(234, 238, 251) 100%)' :
+                'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 25%, rgb(234, 238, 251) 25%, rgb(234, 238, 251) 100%)';
             break;
         case 0:
-            updatedPrice = "$16.00";
+            updatedPrice = isRightToggle ? "$144.00" : "$16.00";
             updatedPageview = '100K';
-            background = 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 50%, rgb(234, 238, 251) 50%, rgb(234, 238, 251) 100%)';
+            background = isRightToggle ? 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 50%, rgb(234, 238, 251) 50%, rgb(234, 238, 251) 100%)' :
+                'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 50%, rgb(234, 238, 251) 50%, rgb(234, 238, 251) 100%)';
             break;
         case 1:
-            updatedPrice = "$24.00";
+            updatedPrice = isRightToggle ? "$216.00" : "$24.00";
             updatedPageview = '500K';
-            background = 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 75%, rgb(234, 238, 251) 75%, rgb(234, 238, 251) 100%)';
+            background = isRightToggle ? 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 75%, rgb(234, 238, 251) 75%, rgb(234, 238, 251) 100%)' :
+                'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 75%, rgb(234, 238, 251) 75%, rgb(234, 238, 251) 100%)';
             break;
         case 2:
-            updatedPrice = "$36.00";
+            updatedPrice = isRightToggle ? "$324.00" : "$36.00";
             updatedPageview = '1M';
-            background = 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 100%, rgb(234, 238, 251) 100%, rgb(234, 238, 251) 100%)';
+            background = isRightToggle ? 'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 100%, rgb(234, 238, 251) 100%, rgb(234, 238, 251) 100%)' :
+                'linear-gradient(to right, rgb(165, 243, 235) 0%, rgb(165, 243, 235) 100%, rgb(234, 238, 251) 100%, rgb(234, 238, 251) 100%)';
             break;
         default:
             updatedPrice = "";
@@ -49,7 +59,3 @@ function updatePrice() {
     pageviewElement.textContent = updatedPageview;
     slider.style.background = background;
 }
-
-toggleBtn.addEventListener('click', () => {
-    toggleBtn.classList.toggle('right');
-});
